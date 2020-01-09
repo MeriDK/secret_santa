c1b812a2afc9ef1d3cc3fc63600e4c22914c03bf
Mariia Rizhko <43323923+MeriDK@users.noreply.github.com>
2020-01-09 17:47:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,25 @@
 import random
-
 import telebot
 
 
-TOKEN = '964330098:AAEVEGcVmp4oZTJJ9s2VyyrQQTK9wwd_u8c'
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 
-updates = bot.get_updates()
-for mess in updates:
-    try:
-        print(mess.message.from_user.id, mess.message.from_user.username, mess.message.text)
-    except:
-        print(mess)
-print()
-
-ids = [227190186, 391835327, 641170928, 234245907, 422000390, 384955663]
+ids = []    # telegram users id
 old_ids = [el for el in ids]
 random.shuffle(ids)
+
+# check if user don't get him-self
 count = 0
 while True:
     if True not in [old_ids[i] == ids[i] for i in range(len(ids))]:
         break
     else:
         count += 1
-        old_ids = [el for el in ids]
-        random.shuffle(ids)
+        random.shuffle(old_ids)
 print('oops', count, '\n')
 
+# send messages to secret Santas
 for i in range(len(ids)):
     bot.send_message(old_ids[i], f'Your secret friend is @{bot.get_chat(ids[i]).username}')
 
